cea18d57694023bb6eb0d2fcd2a212f5e82e85ec
Riyel10 <rakibulriyel1171@g,ail.com>
2026-06-11 22:06:42 +0600

    initial commit

--- server/index.js
+++ server/index.js
@@ -13,11 +13,20 @@ const Note = require('./models/Note');
 
 const app = express();
 const server = http.createServer(app);
-const allowedOrigins = process.env.FRONTEND_URL
-  ? process.env.FRONTEND_URL.split(',').map((origin) => origin.trim())
-  : '*';
-const corsOptions = { origin: allowedOrigins };
+const corsOptions = { origin: '*' };
 const io = new Server(server, { cors: corsOptions });
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 app.use(cors(corsOptions));
 app.use(express.json());
